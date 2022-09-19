c111860a0289fcbe478dc6e7b4a5adc457b77419
kawolski <prashantxrawat@gmail.com>
2022-09-19 14:39:23 +0530

    Medium

---
+++ Search In a 2D Matrix.cpp
@@ -0,0 +1,33 @@
+//      Time Complexity  : O(Log(N))
+//      Space Complexity : O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        //  Function to find nth element in 2-d matrix row-wise ordered
+        function<int(int)> get_nth_element = [&](int num)->int{
+            return (*(matrix[num/c].begin()+num%c));
+        };
+        //  Assume element not present
+        bool ans = false;
+        //  Binary Search Compute
+        int low = 0;
+        int high = r*c-1;
+        
+        while(low <= high){
+            int mid = (low + high)>>1;
+            int element = get_nth_element(mid);
+            if(element < target)
+                low = mid + 1;
+            else if(element > target)
+                high = mid - 1;
+            else{
+                ans = true;
+                break;
+            }
+        }
+        //  Return ans
+        return ans;
+    }
+};
