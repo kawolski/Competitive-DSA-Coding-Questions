62289d2cae0f0ced657d1d5ff37907093a449e9e
kawolski <prashantxrawat@gmail.com>
2022-10-06 12:26:32 +0530

    'Easy'

---
+++ LeetCode/Study_Plan_Level_2/Happy_Number.cpp
@@ -0,0 +1,30 @@
+//      Time Complexity  : O()
+//      Space Complexity : O()
+class Solution {
+public:
+    //  Function to square and add all digits of the given number
+    int get_next(int num){
+        int res = 0;
+        while(num){
+            res += pow(num%10,2);
+            num /= 10;
+        }
+        return res;
+    }
+    bool isHappy(int n) {
+        //  To keep track of visited number
+        set<int> visited;
+        
+        //  Keep going until we start to loop again
+        while(n != 1 && visited.count(n) == 0){
+            visited.insert(n);
+            //  Get next number
+            n = get_next(n);
+        }
+        //  Check if we reached 1
+        if(n == 1)
+            return true;
+        else
+            return false;
+    }
+};
