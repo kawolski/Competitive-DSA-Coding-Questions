c0c419cab96f60a4895f8053249a2e7a94a299be
kawolski <prashantxrawat@gmail.com>
2022-10-10 11:49:34 +0530

    'Rearrange'

--- LeetCode/Study_Plan_Level_2/Day_6/Balanced-Binary-Tree.cpp
+++ LeetCode/Study_Plan_Level_2/Day_6/Balanced-Binary-Tree.cpp
@@ -9,8 +9,6 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//      Time Complexity  : O(N)
-//      Space Complexity : O(N)
 class Solution {
 public:
     //  Function to find height of the Node/Subtree
@@ -23,7 +21,33 @@ public:
         //  Return 1 + longest tail will be considered for determining height
         return 1 + max(left,right);
     }
+    int solve(TreeNode* root,int &maxDiff){
+        if(root == nullptr) {
+            maxDiff = max(maxDiff,0);   return 0;
+        }
+        if(root->left == nullptr && root->right == nullptr) {
+            maxDiff = max(maxDiff,0);   return 1;
+        }
+        //  Find the difference of left & right subtree height
+        int left = solve(root->left,maxDiff);
+        int right = solve(root->right,maxDiff);
+        //  Update maxDifference for current node
+        maxDiff = max(maxDiff,abs(left-right));
+        //  Return max height of subtree, left or right, and include current node
+        return max(left,right)+1;
+    }
     bool isBalanced(TreeNode* root) {
+        //  Solve By :
+        //  Time Complexity  : O(N)
+        //  Space Complexity : O(log N)
+        int maxDiff = INT_MIN;
+        solve(root,maxDiff);
+        if(maxDiff < 2) return true;
+        else                        return false;
+        
+        //  Solve By :
+        //  Time Complexity  : O(N^2)
+        //  Space Complexity : O(log N)
         //  Base case
         if(root == nullptr) return true;
         //  Find height of left sub-tree
