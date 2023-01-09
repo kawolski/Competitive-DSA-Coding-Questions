e868926812f27c24dc025f25848b24bc10c19609
kawolski <prashantxrawat@gmail.com>
2023-01-09 11:07:30 +0530

    Medium

---
+++ LeetCode/Study_Plan_Level_2/Day_18/Asteroid_Collision.py
@@ -0,0 +1,19 @@
+#       Time Complexity  : O(N)
+#       Space Complexity : O(N)
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for a in asteroids :
+            while stack and a < 0 and stack[-1] > 0 :
+                diff = a + stack[-1]
+                if diff < 0 :
+                    stack.pop()
+                elif diff > 0 :
+                    a = 0
+                else :
+                    a = 0
+                    stack.pop()
+            if a :
+                stack.append(a)
+        return stack
